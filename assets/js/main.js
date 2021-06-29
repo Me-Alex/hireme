d992d804605a9f4ec46436344669c841ee4a537d
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2021-06-29 14:41:40 +0300

    improved styling,animation. added contact.html , now more organizated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,28 @@
-const dropDown = document.querySelector(".drop-down");
-const toDrop = document.querySelector(".to-drop");
+const dropDown = document.querySelectorAll(".drop-down");
+const toDrop = document.querySelectorAll(".to-drop");
 console.log("haide aba");
 
-const styling = toDrop.style.display;
 
-
-dropDown.onclick = () => {
-    if (toDrop.style.display == "grid") {
-        toDrop.style.display = "none";
-
-    } else {
-        toDrop.style.display = "grid";
-        toDrop.style.justifyContent = "center";
-        toDrop.style.alignItems = "center";
-
-    }
-
+console.log(dropDown);
+// hides the dropdown menue
+dropDown[0].onclick = () => {
+    toDrop[0].classList.toggle("show");
 }
+dropDown[1].onclick = () => {
+    toDrop[1].classList.toggle("show");
+}
+window.onclick = function (e) {
+    if (!e.target.matches('.dropper')) {
+        var myDropdown = document.getElementById("22");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+//dropdown hide  ends here
+console.log(document.body);
 
+//changes websites theme
 const dark = document.getElementById("1");
 const light = document.getElementById("2");
 const solar = document.getElementById("3");
@@ -45,3 +50,26 @@ solar.onclick = () => {
 
 
 };
+//theme changer ends here
+
+const on = document.getElementById('On');
+const off = document.getElementById('Off');
+
+on.onclick=()=>{
+    
+}
+
+
+
+let text = document.querySelectorAll(".text");
+console.log(text);
+let lungime = text[0].children.length;
+let array = text[0].children;
+console.log(array);
+// for (let i of array) {
+//     i.style.animationName = "mareste";
+//     i.style.animationDuration = "3s";
+// }
+
+
+// console.log(text.style.animationName);
